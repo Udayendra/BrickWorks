6b7545e1b0100442510792eb2c1479d061d72d4d
Udayendra Pal <109019105+Udayendra@users.noreply.github.com>
2025-06-24 12:13:26 +0530

    commite

--- frontend/src/components/common/http.jsx
+++ frontend/src/components/common/http.jsx
@@ -1,9 +1,13 @@
 
 
-export const apiUrl = "http://localhost:9010/api/";
-export const imageUrl = "http://localhost:9010/uploads/services/";
-export const serviceImageUrl = "http://localhost:9010/uploads/services/";
-export const projectImageUrl = "http://localhost:9010/uploads/projects/";
+export const apiUrl = "https://brickworks-api.onrender.com/api/";
+export const imageUrl = "https://brickworks-api.onrender.com/uploads/services/";
+export const serviceImageUrl = "https://brickworks-api.onrender.com/uploads/services/";
+export const projectImageUrl = "https://brickworks-api.onrender.com/uploads/projects/";
+// export const apiUrl = "http://localhost:9010/api/";
+// export const imageUrl = "http://localhost:9010/uploads/services/";
+// export const serviceImageUrl = "http://localhost:9010/uploads/services/";
+// export const projectImageUrl = "http://localhost:9010/uploads/projects/";
 // export const apiUrl = "https://brickworksbackend.infinityfreeapp.com/api/";
 // export const imageUrl = "https://brickworksbackend.infinityfreeapp.com/uploads/services/";
 // export const serviceImageUrl = "https://brickworksbackend.infinityfreeapp.com/uploads/services/";
